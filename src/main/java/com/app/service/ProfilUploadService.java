419efb97865cd9b28a943a2861aa6f8ed909bba5
jo <yeope8@gmail.com>
2023-11-13 13:29:00 +0900

    회원가입 시 프로필이미지 업로드 가능하게..

---
+++ src/main/java/com/app/service/ProfilUploadService.java
@@ -0,0 +1,75 @@
+package com.app.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProfilUploadService {
+	
+	
+	final String endPoint = "https://kr.object.ncloudstorage.com";
+	final String regionName = "kr-standard";
+	final String accessKey = "4709021E30889EA71430";
+	final String secretKey = "502BB99B1FF4B5B0385E8214586B9BDF25FBB23C";
+	String bucketName = "team2-bucket";
+
+	// S3 client
+	final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+	    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
+	    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+	    .build();
+	
+    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+    	  // UUID로 파일명 생성
+        String uuid = UUID.randomUUID().toString();
+        String fileName = uuid + ".png";
+
+        // 폴더 위치 지정
+        String fileKey = dirName + "/" + fileName;
+        // 파일 URL 생성
+        String fileUrl = getBucketFileUrl(fileKey);
+        
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setContentType(multipartFile.getContentType());
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, dirName + "/" + fileName, multipartFile.getInputStream(), metadata).withCannedAcl(CannedAccessControlList.PublicRead);
+
+    	try {
+    	    s3.putObject(putObjectRequest);
+    	    System.out.format("Folder %s has been created.\n", dirName);
+    	} catch (AmazonS3Exception e) {
+    	    e.printStackTrace();
+    	} catch(SdkClientException e) {
+    	    e.printStackTrace();
+    	}
+    	return fileUrl;
+    }
+    
+    public String getBucketFileUrl(String key) {
+        // 버킷 파일의 URL을 생성
+    	return s3.getUrl(bucketName, key).toString();
+    }
+
+}
